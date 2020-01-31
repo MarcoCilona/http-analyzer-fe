8ccf618154133fec079f0755b7254cc1f72fde25
a03u <marco.cilona@gft.com>
2020-01-31 22:23:19 +0100

    improved BottomSheet swype

--- src/directives.ts
+++ src/directives.ts
@@ -3,9 +3,14 @@ import Vue from 'vue';
 const swipe = Vue.directive('swipe', {
   bind: (el: HTMLElement, binding: any) => {
     let startingPosition: number = 0;
+    let currentPosition: number = 0;
+    let endPosition: number = 0;
     let difference: number = 0;
     const bottomOffset: number = 50;
     const elementStartingPosition: number = window.innerHeight - bottomOffset;
+
+    let touchStartTime: number = 0;
+    let touchEndTime: number = 0;
 
     // eslint-disable-next-line no-param-reassign
     el.style.top = `${elementStartingPosition}px`;
@@ -18,7 +23,25 @@ const swipe = Vue.directive('swipe', {
       el.style.top = '0px';
     });
 
+    el.addEventListener('touchend', ($event: TouchEvent) => {
+      $event.preventDefault();
+      touchEndTime = new Date().getTime();
+
+      // eslint-disable-next-line no-param-reassign
+      el.style.transition = 'top 1s ease-in-out';
+      if (touchEndTime - touchStartTime < 150) {
+        if (endPosition < startingPosition) {
+          // eslint-disable-next-line no-param-reassign
+          el.style.top = '0px';
+        } else if (endPosition > startingPosition) {
+          // eslint-disable-next-line no-param-reassign
+          el.style.top = `${elementStartingPosition}px`;
+        }
+      }
+    });
+
     el.addEventListener('touchstart', ($event: TouchEvent) => {
+      touchStartTime = new Date().getTime();
       // eslint-disable-next-line no-param-reassign
       el.style.transition = '';
       startingPosition = $event.touches[0].clientY;
@@ -28,8 +51,9 @@ const swipe = Vue.directive('swipe', {
 
     el.addEventListener('touchmove', ($event: TouchEvent) => {
       $event.preventDefault();
-      difference = startingPosition - $event.touches[0].clientY;
-      startingPosition = $event.touches[0].clientY;
+      endPosition = $event.touches[0].clientY;
+      difference = currentPosition - endPosition;
+      currentPosition = $event.touches[0].clientY;
       const currentTop: number = el.getBoundingClientRect().top;
       const newTopPosition: number = currentTop - difference;
 
